10baca5f4b43cdc7048208a5ce1f4134f2d932c0
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-06-29 19:52:58 -0300

    Update exe01.py

--- python3/exe01.py
+++ python3/exe01.py
@@ -3,4 +3,16 @@ Elaborar um algoritmo em pseudocódigo que efetue a leitura de um número inteir
 apresente uma mensagem no console informando se esse número é par ou ı́mpar.*#
 
 """
-print("Olá digite um nu")
+#variaveis e leitura 
+num = int(input("Digite um numero : "))
+
+if num > 0: 
+    if (num % 2) : 
+        print("O numero (", num, ") É par !")
+    else:
+        print("O numero (", num, ") É impar !")
+        
+elif num == 0:
+    print("Você digitou o numero", num)
+else:
+    print("O numero que você digitou é negativo ! ")
