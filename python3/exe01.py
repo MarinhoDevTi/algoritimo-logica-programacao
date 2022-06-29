5da4bd0489046d1f30a6a30a50fa7cca26d3b404
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-06-29 19:08:18 -0300

    Create exe01.py

---
+++ python3/exe01.py
@@ -0,0 +1,5 @@
+"""
+Elaborar um algoritmo em pseudocódigo que efetue a leitura de um número inteiro positivo e, em seguida
+apresente uma mensagem no console informando se esse número é par ou ı́mpar.*#
+
+"""
