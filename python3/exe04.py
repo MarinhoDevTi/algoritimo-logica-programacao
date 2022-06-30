0a5030f43185f8ada898840e61b8374e0a423d18
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-06-29 21:31:08 -0300

    Update exe04.py

--- python3/exe04.py
+++ python3/exe04.py
@@ -14,3 +14,29 @@ valor02 = float(input("Digite o Primeiro Valor: "))
 valor03 = float(input("Digite o Primeiro Valor: "))
 valor04 = float(input("Digite o Primeiro Valor: "))
 valor05 = float(input("Digite o Primeiro Valor: "))
+print("-------------------------------------------------------")
+
+#calcula media
+media = (valor01 + valor02 + valor03 + valor04 + valor05) / 5
+print("Este é o Resultado da Média", media)
+print("-------------------------------------------------------")
+
+
+#saida de valores ...
+print("Os valores logo abaixo são maiores que a média !")
+if valor01 > media : 
+    print(valor01, ",")
+if valor02 > media : 
+    print(valor02, ",")
+if valor03 > media :
+    print(valor03, ",")
+if valor04 > media :
+    print(valor04, ",")    
+if valor05 > media :
+    print(valor05, ",")
+    
+
+            
+            
+    
+
