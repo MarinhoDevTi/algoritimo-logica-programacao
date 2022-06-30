cee9b8e7433f4d6b0f619a2503c726485faaca85
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-06-29 21:08:50 -0300

    Create exe04.py

---
+++ python3/exe04.py
@@ -0,0 +1,16 @@
+"""
+Escrever um algoritmo para ler cinco valores inteiros, calcular a sua média, e escrever na tela os números
+que são superiores à média calculada
+
+"""
+
+#inicio
+print("+======= ..::Vamos calcular a Média::.. ========+")
+print("=================================================")
+
+#declaração de variaveis e leitura de dados ...
+valor01 = float(input("Digite o Primeiro Valor: "))
+valor02 = float(input("Digite o Primeiro Valor: "))
+valor03 = float(input("Digite o Primeiro Valor: "))
+valor04 = float(input("Digite o Primeiro Valor: "))
+valor05 = float(input("Digite o Primeiro Valor: "))
