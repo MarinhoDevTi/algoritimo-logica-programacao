825148a48743a75e2ff6c18b26bb455567df0a0c
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-05 20:09:05 -0300

    Update

---
+++ PROVAS/prova_03-final_06.07.2022/pergunta_01/pergunta01.py
@@ -0,0 +1,13 @@
+"""
+Escreva um algoritmo para calcular a área de um triângulo e que não permita a 
+entrada de dados inválidos, ou seja, medidas menores ou iguais a 0. 
+Para entradas inválidas, uma mensagem deve ser apresentada em tela para o usuário.
+
+Obs.: sua resposta deve contemplar  
+
+o fluxograma
+pseudocódigo em VisuAlg
+código-fonte em Python3
+a entrega deve ser realizada via arquivo compactado ( .zip )
+
+"""
