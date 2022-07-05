28c7dcee27f1cddc64e530dbd14694894fb645ae
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-05 00:37:31 -0300

    Create pergunta01.py

---
+++ TRABALHOS/trabalho02_06.07.2022/pergunta01.py
@@ -0,0 +1,9 @@
+"""
+Escreva um algoritmo, em VisuAlg, que preencha uma matriz 5 x 5 com números reais e 
+encontre o maior valor da matriz. A seguir, o algoritmo deverá multiplicar cada elemento 
+da diagonal principal pelo maior valor encontrado e mostrar a matriz resultante após as 
+multiplicações.
+
+Exemplo:
+
+"""
