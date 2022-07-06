709ff56d58c0f5ae1cb5b568f5f2beca91c7de53
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-05 21:57:11 -0300

    Update

--- PROVAS/prova_03-final_06.07.2022/pergunta_01/pergunta01.py
+++ PROVAS/prova_03-final_06.07.2022/pergunta_01/pergunta01.py
@@ -12,49 +12,25 @@ a entrega deve ser realizada via arquivo compactado ( .zip )
 
 """
 
-algoritmo "Pergunta 01"
+#inicio e leitura de dados ...
+print("....::: Vamos calcular a area do Triangulo :::....")
+print("Vamos começar !!!")
+print("===================================================")
+bas = int(input(f"Digite o Valor da Base: "))
+print("----------------------------------------------------")
+alt = int(input(f"Digite o Valor da Altura: "))
+print("----------------------------------------------------")
 
-// Autor : "Diogo Marinho da Silva 
-// Data : 05/07/2022
-// Escreva um algoritmo para calcular a area de um triangulo e que nao permita
-// a entrada de dados inválidos, ou seja, medidas menores ou iguais a 0. 
-// Para entradas inválidas, uma mensagem deve ser apresentada em tela 
-// para o usuário.
 
-// Obs.: sua resposta deve contemplar  
+#condições ...
+if (bas <= 0) or (alt <= 0):
+    print("!!! Erro !!! Você digitou um numero negativo !")
+    print("===================================================")
+else:
+    are = (bas * alt) / 2
+    print("Esta e a area do triangulo: ", are)
+    print("===================================================")
 
-// * o fluxograma
-// * pseudocódigo em VisuAlg
-// * código-fonte em Python3
-// * a entrega deve ser realizada via arquivo compactado ( .zip )
-
-#vou faer
-
-// Declaracoes de variaveis ...
-var
-bas, alt, are : real
-
-inicio
-    escreval("....::: Vamos calcular a area do Triangulo :::....")
-    escreval("Vamos começar !!!")
-    escreval("===================================================")
-    escreva("Digite o valor da base: ")
-    leia (bas)
-    escreval("")
-    escreval("----------------------------------------------------")
     
-    escreva("Digite o valor da altura: ")
-    leia (alt)
-    escreval("")
-    escreval("----------------------------------------------------")
 
-    se (bas <= 0) ou (alt <= 0) entao
-        escreval (" !! Erro !! Voce digitou um numero ")
-        escreval("===================================================")
-    senao
-        are <- ( bas * alt) / 2
-        Escreval ("Esta e a area do triangulo: ", are) 
-        escreval("===================================================")   
-    fimse
- 
-fimalgoritmo
+    
