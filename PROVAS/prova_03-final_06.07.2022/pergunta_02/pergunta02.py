c683078ee44e5ad1d8cd7a8055bd4d11e7b7a1e4
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-06 21:39:55 -0300

    Update Material

---
+++ PROVAS/prova_03-final_06.07.2022/pergunta_02/pergunta02.py
@@ -0,0 +1,17 @@
+"""
+Escreva um algoritmo que apresente o menu de opções a seguir:
+
+
+Menu de opções:
+1. Média aritmética 
+2. Média ponderada 
+3. Sair 
+Digite a opção desejada: ______
+
+
+Na opção 1: receber duas notas, calcular e mostrar a média aritmética.
+Na opção 2: receber três notas e seus respectivos pesos, calcular e mostrar a média ponderada.
+Na opção 3: sair do programa.
+Verifique a possibilidade de opção inválida. Nesse caso, o programa deverá mostrar uma mensagem
+
+"""
