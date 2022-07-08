84eef6ddaadcd61817a4b5ec348a2d4919ccf4b4
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-07 23:24:31 -0300

    Update pergunta02.py

--- PROVAS/prova_03-final_06.07.2022/pergunta_02/pergunta02.py
+++ PROVAS/prova_03-final_06.07.2022/pergunta_02/pergunta02.py
@@ -15,35 +15,71 @@ Na opção 3: sair do programa.
 Verifique a possibilidade de opção inválida. Nesse caso, o programa deverá mostrar uma mensagem
 
 """
-
-while True: 
-    print("Menu de opcoes: ")
-    print(" 1- Media aritimetica! ")
-    print(" 2- Media ponderada! ")
-    print(" 3- Sair! ")
-    op = int(input("Qual a Opcao desejada? "))
+def linha():
     print('-='*30)
-    if op in (1, 2, 3):
-        if op == 1:
-            print('voce ira digitardois valores')
-            n1 = float(input('digite o valor 1: '))
-            n2 = float(input('digite o valor 2: '))
-            print(f'A media aritimetica do valor {n1} e {n2} e: {(n1+n2)/2:.2f}')
-            
-        elif op == 2:
-            print('voce ira digitar tres valores')
-            n1 = float(input('digite o primeiro valor 1: '))
-            p1 =float(input('digite o valor do peso 1: '))
-            n2 = float(input('digite o segundo valor 2: '))
-            p2 =float(input('digite o valor do peso 2: '))
-            n3 = float(input('digite o terceiro valor 3: '))
-            p3 =float(input('digite o valor do peso 3: '))
-            print('-*' * 30)
-            print('formula usada: MP = (((n1*p1)+(n2*p2)+(n3*p3))/(p1*p2*p3))')
-            print('-*' * 30)
-            print('valor 1: {n1}, peso: {p1} -- valor 2: {n2}, {p2} -- valor 3: {n3}, peso: {p3}')
-            print('-*' * 30)
-            print(f'A media ponderada e: {(((n1*p1) + (n2*p2) + (n3*p3))/(p1+p2+p3)):.2f}')
-        else:
-            print('voce digitou para sair!')
-            break
+#fim da função linha()
+
+def mensagem(msg):
+    print("-=-", * 40)
+    print(msg)
+    print("-=-", * 40)
+#fim função mensagem(msg)
+    
+mensagem("      ..::MENU DE OPÇÕES::..      ")
+
+print("  1.  Média Aritmética")
+print("  2.  Média Ponderada")
+print("  3.  Sair")
+print('')
+opcao = int(input("Informe uma opção: "))
+
+if opcao < 1 or opcao > 3:
+    print('')
+    mensagem("    Essa opção é INVÁLIDA! Tente Novamente !")
+    
+
+if opcao == 1:
+    print('')
+    mensagem("    Média Aritmética    ")    
+
+    not_01 = float(input("Informe a primeira nota: "))
+    not_02 = float(input("Informe a segunda nota: "))
+    print('')
+    media = (not_01 + not_02) / 2
+
+    mensagem("   A média aritmética é: ",media)    
+
+elif opcao == 2:
+    print('')
+
+    mensagem("     Média Ponderada")    
+
+    not_01 = float(input("Informe a primeira nota: "))
+    peso1 = float(input("Informe o peso da primeira nota: "))
+    linha()
+    print('')
+
+    not_02 = float(input("Informe a segunda nota: "))
+    peso2 = float(input("Informe o peso da segunda nota: "))
+    linha()
+    print('')
+
+    nota3 = float(input("Informe a terceira nota: "))
+    peso3 = float(input("Informe o peso da terceira nota: "))
+    linh()
+    print('')
+
+    mediaP = (nota1*peso1) + (nota2*peso2) + (nota3*peso3) / (peso1 + peso2 + peso3)
+    print("A Média Ponderada é: ",mediaP)
+
+elif opcao == 3:
+    mensagem("PROGRAMA ENCERRADO")
+    
+
+    
+
+
+
+
+
+
